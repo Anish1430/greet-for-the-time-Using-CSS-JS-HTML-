b2e86198cc502b1f7f641d92597b8ab8541915cc
Anish Kumar Sharma <80304547+Anish1430@users.noreply.github.com>
2021-11-24 20:31:05 +0530

    Add files via upload

---
+++ greeting.js
@@ -0,0 +1,15 @@
+function gm() {
+    var h1ref=document.getElementById("h1");
+    h1ref.innerText="Good Morning ";
+    h1ref.style.color="tomato";
+}
+function ge() {
+    var h1ref = document.getElementById("h1");
+    h1ref.innerText="Good AfterNoon ";
+    h1ref.style.color="yellow";  
+}   
+function gn(){
+    var h1ref = document.getElementById("h1");
+    h1ref.innerText="Good Night ";
+    h1ref.style.color="red";     
+}
